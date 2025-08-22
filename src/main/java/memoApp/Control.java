c5f67a4aa8c04c6524a2859b5d384196c569e66a
azu-wakinoza <wakinoza@gmail.com>
2025-08-22 16:13:43 +0900

    Controlクラスにjavadocを記載

--- src/main/java/memoApp/Control.java
+++ src/main/java/memoApp/Control.java
@@ -4,15 +4,30 @@ import java.io.IOException;
 import java.util.List;
 import javax.swing.JOptionPane;
 
+/**.
+ * ModelクラスとViewクラスのかけ渡しを担うクラス
+ */
 public class Control {
+  /**.Modelクラスのインスタンスへの参照*/
   private final Model MODEL;
+
+  /**.Viewクラスのインスタンスへの参照*/
   private final View VIEW;
 
+  /**.
+   * コンストラクタ
+   *
+   * @param model Modelクラスのインスタンスへの参照
+   * @param view Viewクラスのインスタンスへの参照
+   */
   public Control(Model model, View view) {
     this.MODEL = model;
     this.VIEW = view;
   }
 
+  /**.
+   * ボタン操作の処理を指示するメソッド
+   */
   public void setupListeners() {
 
     //保存ボタン
@@ -37,9 +52,7 @@ public class Control {
     });
 
     //テキスト削除ボタン
-    VIEW.getTextClearButton().addActionListener(e -> {
-      VIEW.getMemoTextArea().setText("");
-    });
+    VIEW.getTextClearButton().addActionListener(e -> VIEW.getMemoTextArea().setText(""));
 
     //再編集ボタン
     VIEW.getEditButton().addActionListener(e -> {
@@ -67,6 +80,9 @@ public class Control {
     });
   }
 
+  /**.
+   * 履歴表示の再構成を指示するメソッド
+   */
   public void updateHistoryPanel() {
     List<String> fileNames = MODEL.getHistoryList();
     VIEW.updateHistoryPanel(fileNames);
